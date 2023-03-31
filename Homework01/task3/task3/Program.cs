bd6226d33d44fc6c91df4c65d5c6fd84e690631e
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-03-31 09:05:32 +0500

    new code ready

--- Homework01/task3/task3/Program.cs
+++ Homework01/task3/task3/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Enter number : ");
+            int numbe1 = int.Parse(Console.ReadLine());
+            Console.Write(" Enter number : ");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.Write(" Enter number : ");
+            int number3 = int.Parse(Console.ReadLine());
+            Console.Write(" Enter number : ");
+            int number4 = int.Parse(Console.ReadLine());
+            Console.Write(" The smallest of numbers : ");
+            Console.WriteLine(Math.Max(Math.Max(numbe1, number2), Math.Max(number3, number4))); 
+
         }
     }
 }
