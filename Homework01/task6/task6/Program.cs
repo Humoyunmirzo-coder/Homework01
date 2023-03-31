9b60af52bf5cc326e98766a0d7c9f8e63b6f8cb2
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-03-31 09:20:43 +0500

    Update Program.cs

--- Homework01/task6/task6/Program.cs
+++ Homework01/task6/task6/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" Times Table!  ");
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($" {i} * {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
