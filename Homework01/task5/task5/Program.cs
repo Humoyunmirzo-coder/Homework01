a7154f2a2ac2f1d88fbc764ee97942a4ea2b60cb
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-03-31 09:16:14 +0500

    code

--- Homework01/task5/task5/Program.cs
+++ Homework01/task5/task5/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+
+            //    THIS IS CODE NOT YET  FULL
+
             Console.WriteLine(" Enter number  ");
             int max = 0;
             int number = int.Parse(Console.ReadLine());
