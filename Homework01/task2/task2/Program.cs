0029c1c6fbc578c77b25e4b56d5cb3df8f2f2d7f
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-03-31 08:56:55 +0500

    new code

--- Homework01/task2/task2/Program.cs
+++ Homework01/task2/task2/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numeral;
+            Console.WriteLine(" Enter number");
+            int.TryParse(Console.ReadLine(), out numeral);
+
+            if (numeral == 0)
+            {
+                Console.WriteLine(" This is number defauld has value !");
+            }
+            for (int i = 0; i < numeral; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
