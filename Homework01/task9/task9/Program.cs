2f2fa8988ba7f0987f35080aee44878147e60b81
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-03-31 09:29:30 +0500

    .

---
+++ Homework01/task9/task9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int box = 0;
+            Console.WriteLine(" Enter number ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    box++;
+                    Console.WriteLine(i);
+                }
+            }
+            if (box == 2)
+            {
+                Console.WriteLine(" This is a prime number");
+            }
+            else
+            {
+                Console.WriteLine(" It is not a  prime   nomber");
+            }
+        }
+    }
+}
